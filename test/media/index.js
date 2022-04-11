cc300d550b2064aa4cf6a95bc94d7b0b4c4a7ce6
flexrouting+ci <flexrouting+ci@twilio.com>
2022-04-11 07:45:21 +0000

    0.5.10

--- test/media/index.js
+++
@@ -1,8 +0,0 @@
-require('../voice/inbound/spec/Reservation');
-require('../voice/inbound/spec/Supervisor');
-require('../voice/outbound/spec/Reservation');
-require('../voice/outbound/spec/Supervisor');
-require('../voice/outbound/spec/Task');
-require('../voice/outbound/spec/TaskTransferToQueue');
-require('../voice/outbound/spec/TaskTransferToWorker');
-require('../voice/outbound/spec/ExternalTransfer');
